6a23854f2c8c0237b84183aa6cae335b9cfd8f97
prodbysky <kyrbynyt@gmail.com>
2024-08-10 21:34:08 +0300

    C# decided to change sometihn?

--- c-sharp/obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
+++ c-sharp/obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2d55c7f692a4581836685ef5282b908193f434")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f21d43586cc394068a1013d83f36ac972dc19dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
